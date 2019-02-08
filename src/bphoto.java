40ab147f74d055c3fa8b663fe4d501f77d09658c
slopey112 <howard_peng@icloud.com>
2019-02-07 16:41:39 -0800

    update config

--- src/bphoto.java
+++ src/bphoto.java
@@ -1,35 +1,34 @@
 import java.util.*;
 import java.io.*;
 
-class BinaryIndexTree {
-    private static int[] BITree;
-    private static int n;
-
-    void update(int index) {
-        //Stores 'val' at 'index'.
-        index++;
-        for (; index <= n; index += index & (-index)) {
-            BITree[index]++;
-        }
-    }
-    int getSum(int index) {
-        //Finds prefix sum of index.
-        int sum = 0;
-        index++;
-        for (; index > 0; index -= index & (-index)) {
-            sum += BITree[index];
-        }
-        return sum;
-    }
-    BinaryIndexTree (int N) {
-        //Optional constructor for binary index
-        // tree that allows user to construct values themselves.
-        n = N;
-        BITree = new int[N+1];
-    }
-}
-
 public class bphoto {
+    static class BIT {
+        private static int[] BITree;
+        private static int n;
+
+        void update(int index) {
+            //Stores 'val' at 'index'.
+            index++;
+            for (; index <= n; index += index & (-index)) {
+                BITree[index]++;
+            }
+        }
+        int getSum(int index) {
+            //Finds prefix sum of index.
+            int sum = 0;
+            index++;
+            for (; index > 0; index -= index & (-index)) {
+                sum += BITree[index];
+            }
+            return sum;
+        }
+        BIT (int N) {
+            //Optional constructor for binary index
+            // tree that allows user to construct values themselves.
+            n = N;
+            BITree = new int[N+1];
+        }
+    }
     static class Cow implements Comparable<Cow> {
         private int height, index;
         private Cow (int height, int index) {
@@ -46,7 +45,7 @@ public class bphoto {
         Cow[] cows = new Cow[N];
         for (int i = 0; i < N; i++) cows[i] = new Cow(Integer.parseInt(in.readLine()), i);
 
-        BinaryIndexTree sums = new BinaryIndexTree(N);
+        BIT sums = new BIT(N);
         Arrays.sort(cows);
         int ans = 0, c = 0;
         for (Cow now : cows) {
